8baf80716d3e62ddc42ea7792d49f6f719b7753f
alex fielder <alex.fielder@gmail.com>
2019-03-15 14:46:40 +0000

    Update create 7z containing updated files only.cmd

--- create 7z containing updated files only.cmd
+++ create 7z containing updated files only.cmd
@@ -1,2 +1,2 @@
-7z u "{drive}:\{path}\{to}\{first}\{backup}\{in}\{sequence}\{*.7z}" -up0q3x2z0!"{drive}:\{path}\{to}\{new}\{backup}\{in}\{sequence}\{*.7z}" "{drive}:\{path}\{to}\scan\*" -ms=off
+7z u "{drive}:\{path}\{to}\{first}\{backup}\{in}\{sequence}\{*.7z}" -u- -up0q3x2z0!"{drive}:\{path}\{to}\{new}\{backup}\{in}\{sequence}\{*.7z}" "{drive}:\{path}\{to}\scan\*" -ms=off
 REM 7z u "c:\temp\7z test\udated-2019-03-15.7z" -up0q3x2z0!"c:\temp\7z test\update-2019-03-15-001.7z" "c:\temp\7z test\Repos\*" -ms=off
