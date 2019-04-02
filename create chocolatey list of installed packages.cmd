6314d94505f2caa1fb75aeaf1031ad7f5ab3871b
Alex Fielder <alex.fielder@gmail.com>
2019-04-02 20:36:04 +0100

    removed time as it screws the filepath

--- create chocolatey list of installed packages.cmd
+++ create chocolatey list of installed packages.cmd
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 REM setlocal enableDelayedExpansion
 SET APPCONFIGPATH=\App config files\Chocolatey (%computername%)\installed-%DATE%.log
 for %%? in ("%~dp0..") do set parent=%%~f?
@@ -7,7 +7,7 @@ REM echo/%APPCONFIGPATH% is your output path
 IF NOT EXIST "%parent%\App config files\Chocolatey (%computername%)" (MKDIR "%parent%\App config files\Chocolatey (%computername%)")
 echo output folder is: "%parent%\App config files\Chocolatey (%computername%)"
 REM SET OUTPUT="%parent%\App config files\Chocolatey (%computername%)\installed-%DATE%-%TIME%.log"
-echo outputting data to: "%parent%\App config files\Chocolatey (%computername%)\installed-%DATE%-%TIME%.log"
-choco list -lo > "%parent%\App config files\Chocolatey (%computername%)\installed-%DATE%-%TIME%.log"
-echo outputting reinstall script to: '%parent%\App config files\Chocolatey (%computername%)\install-script-%DATE%-%TIME%.ps1'
-powershell -command "& '.\Generate_Choco_Install_script.ps1' '%parent%\App config files\Chocolatey (%computername%)\install-script-%DATE%-%TIME%.ps1'"
+echo outputting data to: "%parent%\App config files\Chocolatey (%computername%)\installed-%DATE%.log"
+choco list -lo > "%parent%\App config files\Chocolatey (%computername%)\installed-%DATE%.log"
+echo outputting reinstall script to: '%parent%\App config files\Chocolatey (%computername%)\install-script-%DATE%.ps1'
+powershell -command "& '.\Generate_Choco_Install_script.ps1' '%parent%\App config files\Chocolatey (%computername%)\install-script-%DATE%.ps1'"
