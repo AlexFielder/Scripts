18e57cf3ea6ea201f27acce99146bd5efaa446f9
Alex Fielder <alex.fielder@gmail.com>
2019-04-24 20:57:43 +0100

    amended to allow script execution on machine where policy hasn't been overridden

--- create chocolatey list of installed packages.cmd
+++ create chocolatey list of installed packages.cmd
@@ -10,4 +10,4 @@ REM SET OUTPUT="%parent%\App config files\Chocolatey (%computername%)\installed-
 echo outputting data to: "%parent%\App config files\Chocolatey (%computername%)\installed-%DATE%.log"
 choco list -lo > "%parent%\App config files\Chocolatey (%computername%)\installed-%DATE%.log"
 echo outputting reinstall script to: '%parent%\App config files\Chocolatey (%computername%)\install-script-%DATE%.ps1'
-powershell -command "& '.\Generate_Choco_Install_script.ps1' '%parent%\App config files\Chocolatey (%computername%)\install-script-%DATE%.ps1'"
+powershell -ExecutionPolicy bypass -command "& '.\Generate_Choco_Install_script.ps1' '%parent%\App config files\Chocolatey (%computername%)\install-script-%DATE%.ps1'"
