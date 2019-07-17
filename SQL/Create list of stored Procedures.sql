c55dd5a1b63bbda0f2e21cec79f7401638cb1b4a
alex fielder <alex.fielder@graitec.com>
2019-07-17 16:55:22 +0100

    filter by datemodified

--- SQL/Create list of stored Procedures.sql
+++ SQL/Create list of stored Procedures.sql
@@ -4,13 +4,13 @@ CREATE TABLE #ListOfSPs
         DBName varchar(100), 
         [OBJECT_ID] INT,
         SPName varchar(100),
-		create_date Date,
 		modify_date Date
     )
 
-    EXEC sp_msforeachdb 'USE [?]; INSERT INTO #ListOfSPs Select ''?'', Object_Id, Name, modify_date, create_date FROM sys.procedures'
+    EXEC sp_msforeachdb 'USE [?]; INSERT INTO #ListOfSPs Select ''?'', Object_Id, Name, modify_date FROM sys.procedures'
 
     SELECT 
         * 
     FROM #ListOfSPs
+	WHERE year(modify_date) = 2019
 	ORDER BY [DBName], [SPName]
