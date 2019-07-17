d18cd9208cbfbab3be1efbfd192cd7cc55119e23
alex fielder <alex.fielder@graitec.com>
2019-07-17 15:27:29 +0100

    added modify_date

--- SQL/Create list of stored Procedures.sql
+++ SQL/Create list of stored Procedures.sql
@@ -3,10 +3,11 @@ CREATE TABLE #ListOfSPs
     (
         DBName varchar(100), 
         [OBJECT_ID] INT,
-        SPName varchar(100)
+        SPName varchar(100),
+		modify_date Date
     )
 
-    EXEC sp_msforeachdb 'USE [?]; INSERT INTO #ListOfSPs Select ''?'', Object_Id, Name FROM sys.procedures'
+    EXEC sp_msforeachdb 'USE [?]; INSERT INTO #ListOfSPs Select ''?'', Object_Id, Name, modify_date FROM sys.procedures'
 
     SELECT 
         * 
