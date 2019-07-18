220f754e23814c4dfd17e7e8faf1824b75eab73f
alex fielder <alex.fielder@graitec.com>
2019-07-18 14:47:59 +0100

    Update checkfilexistsandreport.ps1

--- checkfilexistsandreport.ps1
+++ checkfilexistsandreport.ps1
@@ -1,17 +1,16 @@
 [CmdletBinding()] 
 Param( 
-    [String] $InputCsv = "",
-    [String] $OutputCsv = ""
+    [String]$InputCsv = ""
 )
 
 Class fileToVerify {
     [String]$FileName
-    [bool]$FileExists = $false
+    [String]$FileExists = "false"
 }
 if (Test-path($InputCsv)) 
 {
     $filesToCheck = New-Object "System.Collections.Generic.List[fileToVerify]"
-
+    [System.IO.Fileinfo]$csvfile = Get-Item -path $InputCsv
     $csv = Import-Csv $InputCsv
 
     foreach($item in $csv)
@@ -21,20 +20,34 @@ if (Test-path($InputCsv))
         # $file.FileExists = $false
         $filesToCheck.add($file)
     }
-    # Write-Output $filesToCheck.Count
+    Write-output "files to check= "$filesToCheck.Count
     $CheckedFiles = New-Object "System.Collections.Generic.List[fileToVerify]"
+    [int32]$MissingFiles = 0
     foreach($file in $filesToCheck) {
-        Write-Host $file.FileName
-        if (Test-Path($file.FileName))
+        # Write-Host $file.FileName
+        # 'MapiEnabled' = If($item.MapiEnabled.Trim() -eq 'TRUE'){$True} Else{$False}
+        if(Test-Path($file.FileName))
         {
-            $file.$FileExists = $true
+            $file.FileExists = "true"
         }
         else {
-            $file.$FileExists = $false
+            $file.FileExists = "false"
+            $MissingFiles += 1
         }
+        # $file.FileExists = if (Test-Path($file.FileName)) {"true"} Else {"false"}
+        # if ($file.FileExists = "false") {$MissingFiles += 1}
+        # if (Test-Path($file.FileName))
+        # {
+        #     $file.$FileExists = $true
+        # }
+        # else {
+        #     $file.$FileExists = $false
+        # }
         $CheckedFiles.add($file)
     }
-    Write-Output $CheckedFiles.Count
+    Write-output "checked files= "$CheckedFiles.Count"\nmissing files="$MissingFiles
+    [String]$OutputCsv = $csvfile.DirectoryName+"\output.csv"
+    Export-Csv -path $OutputCsv -Delimiter "," -InputObject $CheckedFiles -NoTypeInformation
 }
 
 
