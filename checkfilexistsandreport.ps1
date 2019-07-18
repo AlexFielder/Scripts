6044d5452e0ce97d21e6c6e4ecbe0fd4a4b2d63a
Alex Fielder <alex.fielder@gmail.com>
2019-07-18 13:23:02 +0100

    updates to looping through collections

--- checkfilexistsandreport.ps1
+++ checkfilexistsandreport.ps1
@@ -6,19 +6,35 @@ Param(
 
 Class fileToVerify {
     [String]$FileName
-    [Boolean]$FileExists
+    [bool]$FileExists = $false
 }
-
-$filesToCheck = New-Object "System.Collections.Generic.List[fileToVerify]"
-
-$csv = Import-Csv $InputCsv
-
-foreach($item in $csv)
+if (Test-path($InputCsv)) 
 {
-    $file = New-Object fileToVerify
-    $file.FileName = $item.FileName
-    $file.FileExists = $item.FileExists
-    $filesToCheck.add($file)
+    $filesToCheck = New-Object "System.Collections.Generic.List[fileToVerify]"
+
+    $csv = Import-Csv $InputCsv
+
+    foreach($item in $csv)
+    {
+        $file = New-Object fileToVerify
+        $file.FileName = $item.FileName
+        # $file.FileExists = $false
+        $filesToCheck.add($file)
+    }
+    # Write-Output $filesToCheck.Count
+    $CheckedFiles = New-Object "System.Collections.Generic.List[fileToVerify]"
+    foreach($file in $filesToCheck) {
+        Write-Host $file.FileName
+        if (Test-Path($file.FileName))
+        {
+            $file.$FileExists = $true
+        }
+        else {
+            $file.$FileExists = $false
+        }
+        $CheckedFiles.add($file)
+    }
+    Write-Output $CheckedFiles.Count
 }
 
 
