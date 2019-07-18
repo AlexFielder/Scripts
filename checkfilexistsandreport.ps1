f0653e62aa7d4213c3e9ff88a8a28ca9ecfce920
alex fielder <alex.fielder@graitec.com>
2019-07-18 16:27:15 +0100

    Update checkfilexistsandreport.ps1

--- checkfilexistsandreport.ps1
+++ checkfilexistsandreport.ps1
@@ -1,68 +1,57 @@
-function CheckFileExistsAndReport {
-    [CmdletBinding()]
-    param (
-        [String]$InputCsv = ""
-    )
-    if (Test-path($InputCsv)) 
+[CmdletBinding()]
+param (
+    [String]$InputCsv = ""
+)
+
+#create and start a stopwatch object to measure how long it all takes.
+$stopwatch = [Diagnostics.Stopwatch]::StartNew()
+
+if (Test-path($InputCsv)) 
+{
+    $CheckedFiles = New-Object "System.Collections.Generic.List[fileToVerify]"
+    [System.IO.Fileinfo]$csvfile = Get-Item -path $InputCsv
+    $csv = Import-Csv $InputCsv
+    Write-output "Loading csv into memory"
+    [int32]$MissingFiles = 0
+    foreach($item in $csv)
     {
-        $filesToCheck = New-Object "System.Collections.Generic.List[fileToVerify]"
-        [System.IO.Fileinfo]$csvfile = Get-Item -path $InputCsv
-        $csv = Import-Csv $InputCsv
-
-        foreach($item in $csv)
+        $file = New-Object fileToVerify
+        $file.FileName = $item.FileName
+        if(Test-Path($file.FileName))
         {
-            $file = New-Object fileToVerify
-            $file.FileName = $item.FileName
-            # $file.FileExists = $false
-            $filesToCheck.add($file)
+            $file.FileExists = "true"
         }
-        Write-output "files to check= "$filesToCheck.Count
-        $CheckedFiles = New-Object "System.Collections.Generic.List[fileToVerify]"
-        [int32]$MissingFiles = 0
-        foreach($file in $filesToCheck) {
-            # Write-Host $file.FileName
-            # 'MapiEnabled' = If($item.MapiEnabled.Trim() -eq 'TRUE'){$True} Else{$False}
-            if(Test-Path($file.FileName))
-            {
-                $file.FileExists = "true"
-            }
-            else {
-                $file.FileExists = "false"
-                $MissingFiles += 1
-            }
-            # $file.FileExists = if (Test-Path($file.FileName)) {"true"} Else {"false"}
-            # if ($file.FileExists = "false") {$MissingFiles += 1}
-            # if (Test-Path($file.FileName))
-            # {
-            #     $file.$FileExists = $true
-            # }
-            # else {
-            #     $file.$FileExists = $false
-            # }
-            $CheckedFiles.add($file)
+        else {
+            $file.FileExists = "false"
+            $MissingFiles += 1
         }
-        Write-output "checked files="$CheckedFiles.Count
-        Write-output "missing files="$MissingFiles
-        [String]$OutputCsv = $csvfile.DirectoryName+"\output.csv"
-
-        MakeCSV($CheckedFiles, $OutputCsv)
-        # Export-Csv -path $OutputCsv -Delimiter "," -InputObject $CheckedFiles -NoTypeInformation
+        $CheckedFiles.add($file)
     }
+    [String]$OutputCsv = $csvfile.DirectoryName+"\output.csv"
+    if (!(Test-Path $OutputCsv))
+    {
+        New-Item -path $OutputCsv -type "file"
+    }
+    $CheckedFiles | Export-Csv $OutputCsv -NoTypeInformation
 }
 
- 
+# how long did it all take?
+$stopwatch.stop()
+# $stopwatch
+
+# show what we did.
+[pscustomobject] @{
+    csv_output = $OutputCsv
+    total_files = $CheckedFiles.Count
+    missing_files = $MissingFiles
+    time_taken = $stopwatch.elapsed
+}
+
 Class fileToVerify {
     [String]$FileName
-    [String]$FileExists = "false"
+    [String]$FileExists #= "false"
 }
 
 
-Function MakeCSV($ListObjects,$namefile){
-    $Listobjects | ForEach-Object{
-        [fileToVerify]@{
-            FileName = $_.FileName
-            FileExists = $_.FileExists
-        } | Export-Csv -Path "Output-$NameFile.csv" -NoTypeInformation -Append -Delimiter ","
-    }
-}
+
 
