1ff8f5ccaed3b83580b85035e9a8898c975cfccc
Alex Fielder <alex.fielder@gmail.com>
2019-08-23 14:50:01 +0100

    Modified to take account of reorganisation

--- cmdline/create chocolatey list of installed packages.cmd
+++ cmdline/create chocolatey list of installed packages.cmd
@@ -1,9 +1,9 @@
 @echo on
 REM setlocal enableDelayedExpansion
 SET APPCONFIGPATH=\App config files\Chocolatey (%computername%)\installed-%DATE%.log
-for %%? in ("%~dp0..") do set parent=%%~f?
-REM echo/%parent% is your parent directory
-REM echo/%APPCONFIGPATH% is your output path
+for %%? in ("%~dp0..\..") do set parent=%%~f?
+echo %parent% is your parent directory
+echo %APPCONFIGPATH% is your output path
 IF NOT EXIST "%parent%\App config files\Chocolatey (%computername%)" (MKDIR "%parent%\App config files\Chocolatey (%computername%)")
 echo output folder is: "%parent%\App config files\Chocolatey (%computername%)"
 REM SET OUTPUT="%parent%\App config files\Chocolatey (%computername%)\installed-%DATE%-%TIME%.log"
