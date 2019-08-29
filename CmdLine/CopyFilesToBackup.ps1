8738b68dc366b7e1496c092d23bab67658868a75
Alex Fielder <alex.fielder@gmail.com>
2019-08-29 21:17:13 +0100

    Corrected with help from this thread: https://stackoverflow.com/a/57714345/572634

--- CmdLine/CopyFilesToBackup.ps1
+++ CmdLine/CopyFilesToBackup.ps1
@@ -23,52 +23,11 @@ to run using anything else use this syntax:
 [CmdletBinding()] 
 Param( 
     [String] $FileList = "C:\temp\copytest.csv", 
-    [int] $NumCopyThreads = 8,
+    [int] $NumCopyThreads =75,
     [String] $LogName
 ) 
 
-# $stopwatch = [Diagnostics.Stopwatch]::StartNew()
-
-# $filesToCopy = New-Object "System.Collections.Generic.List[PSCustomObject]"
-# $csv = Import-Csv $FileList
-
-# foreach($item in $csv)
-# {
-#     $fileToCopy = [PSCustomObject]@{
-#         SrcFileName = $item.SrcFileName
-#         DestFileName = $item.DestFileName
-#     }
-#     $filesToCopy.add($fileToCopy)
-# }
-
-# [int32]$FileCount = 0
-
-function copyFileInfo ([PSCustomObject]$file) {
-    [System.IO.FileInfo]$CopyFile = $file.SrcFileName
-    if (-not (Test-path([Management.Automation.WildcardPattern]::Escape($file.DestFileName)))) {
-        $CopyFile.CopyTo($file.DestFileName, $true)
-    }
-}
-
-function isDivisible([int32]$numfiles, [int32]$divisor) {
-    if ($numfiles % $divisor -eq 0) {
-        return $true
-    }
-    else {
-        return $false
-    }
-}
-
-# Import-Csv $FileList | Select-Object SrcFileName, DestFileName | Invoke-All{
-#     [System.IO.Fileinfo]$DestinationFilePath = $_.DestFileName
-#     [String]$DestinationDir = $DestinationFilePath.DirectoryName
-#     if (-not (Test-path([Management.Automation.WildcardPattern]::Escape($DestinationDir)))) {
-#         new-item -Path $DestinationDir -ItemType Directory -Verbose
-#     }
-#     copy-item -path $_.srcFileName -Destination $_.DestFileName -Verbose
-# }
-
-$filesPerBatch = 1000
+$filesPerBatch = 100
 
 $files = Import-Csv $FileList | Select-Object SrcFileName, DestFileName
 
@@ -83,7 +42,7 @@ $jobs = while ($i -lt $files.Count) {
     $fileBatch = $files[$i..$j]
 
     $jobName = "Batch$batch"
-    Start-ThreadJob -Name $jobName -ThrottleLimit $NumCopyThreads -ArgumentList ($fileBatch) -ScriptBlock {
+    Start-ThreadJob -Name $jobName -ThrottleLimit $NumCopyThreads -ArgumentList (,$fileBatch) -ScriptBlock {
         param($filesInBatch)
         foreach ($f in $filesInBatch) {
             [System.IO.Fileinfo]$DestinationFilePath = $f.DestFileName
@@ -92,7 +51,6 @@ $jobs = while ($i -lt $files.Count) {
                 new-item -Path $DestinationDir -ItemType Directory -Verbose
             }
             copy-item -path $f.srcFileName -Destination $f.DestFileName -Verbose
-            "Copied $($f.SrcFileName) to $($f.DestFileName)"
         }
     } 
 
@@ -108,131 +66,3 @@ Write-Host "Waiting for $($jobs.Count) jobs to complete..."
 
 Receive-Job -Job $jobs -Wait -AutoRemoveJob
 Write-Host "Total time lapsed: $([datetime]::UtcNow - $dtStart)"
-
-# Clean up the temp. file
-<# Don't do this!
-Remove-Item $FileList
-#>
-
-
-<# This works but is INCREDIBLY SLOW because it creates a thread per file
-# Create sample CSV file with 10 rows.
-# $FileList = Join-Path ([IO.Path]::GetTempPath()) "tmp.$PID.csv"
-# @'
-# Foo,SrcFileName,DestFileName,Bar
-# 1,c:\tmp\a,\\server\share\a,baz
-# 2,c:\tmp\b,\\server\share\b,baz
-# 3,c:\tmp\c,\\server\share\c,baz
-# 4,c:\tmp\d,\\server\share\d,baz
-# 5,c:\tmp\e,\\server\share\e,baz
-# 6,c:\tmp\f,\\server\share\f,baz
-# 7,c:\tmp\g,\\server\share\g,baz
-# 8,c:\tmp\h,\\server\share\h,baz
-# 9,c:\tmp\i,\\server\share\i,baz
-# 10,c:\tmp\j,\\server\share\j,baz
-# '@ | Set-Content $FileList
-
-# How many threads at most to run concurrently.
-# $NumCopyThreads = 8
-
-Write-Host 'Creating jobs...'
-$dtStart = [datetime]::UtcNow
-
-# Import the CSV data and transform it to [pscustomobject] instances
-# with only .SrcFileName and .DestFileName properties - they take
-# the place of your original [fileToCopy] instances.
-$jobs = Import-Csv $FileList | Select-Object SrcFileName, DestFileName | 
-  ForEach-Object {
-    # Start the thread job for the file pair at hand.
-    Start-ThreadJob -ThrottleLimit $NumCopyThreads -ArgumentList $_ { 
-        param($f) 
-        [System.IO.Fileinfo]$DestinationFilePath = $f.DestFileName
-        [String]$DestinationDir = $DestinationFilePath.DirectoryName
-        if (-not (Test-path([Management.Automation.WildcardPattern]::Escape($DestinationDir)))) {
-            new-item -Path $DestinationDir -ItemType Directory #-Verbose
-        }
-        copy-item -path $f.srcFileName -Destination $f.destFilename
-        "Copied $($f.SrcFileName) to $($f.DestFileName)"
-    }
-  }
-
-Write-Host "Waiting for $($jobs.Count) jobs to complete..."
-
-# Synchronously wait for all jobs (threads) to finish and output their results
-# *as they become available*, then remove the jobs.
-# NOTE: Output will typically NOT be in input order.
-Receive-Job -Job $jobs -Wait -AutoRemoveJob
-Write-Host "Total time lapsed: $([datetime]::UtcNow - $dtStart)"
-
-# Clean up the temp. file
-Remove-Item $FileList
-#>
-
-<# works but can be runspaced? #>
-# if($CopyMethod -eq "sync") {
-#     foreach ($file in $files){ #ToCopy) {
-#         $FileCount += 1
-#         copyFileInfo($file)
-#         <# would probably work if we weren't using a workflow #>
-#         if (isDivisible($FileCount, 1000) -eq $true) {
-#             write-host "Number of files: " + $FileCount
-#         }
-#     }
-# }
-<# Runspaces version #>
-# if ($CopyMethod -eq "Runspace") {
-    # $Runspacepool = [runspacefactory]::CreateRunspacePool(1,$NumCopyThreads)
-    # $Runspacepool.Open()
-
-    # $Runspaces = foreach ($file in $filesToCopy) {
-    #     $PSInstance = [powershell]::Create().AddScript({
-    #         param($destFilename, $srcFileName)
-    #         [System.IO.Fileinfo]$DestinationFilePath = $DestFileName
-    #         [String]$DestinationDir = $DestinationFilePath.DirectoryName
-    #         if (-not (Test-path([Management.Automation.WildcardPattern]::Escape($DestinationDir)))) {
-    #             new-item -Path $DestinationDir -ItemType Directory #-Verbose
-    #         }
-    #         # [System.IO.FileInfo]$CopyFile = $srcFileName
-    #         if (-not (Test-path([Management.Automation.WildcardPattern]::Escape($destFileName)))) {
-    #             copy-item -path $srcFileName -Destination $destFilename
-    #             # $CopyFile.CopyTo($destFileName, $true)
-    #         }
-    #     }).AddParameter('srcFileName', $file.SrcFileName).
-    #     AddParameter('destFileName', $file.DestFileName)
-    #     #Adding Powershell instance to RunspacePool
-    #     $PSInstance.RunspacePool = $Runspacepool
-
-    #     New-Object psobject -Property @{
-    #         instance = $PSInstance
-    #         IAResult = $PSInstance.BeginInvoke()
-    #         Argument = $file
-    #     }
-    # }
-
-    # while($Runspaces |Where-Object{-not $_.IAResult.IsCompleted}) {
-    #     Start-Sleep -Milliseconds 5
-    # }
-
-    # $Results = $Runspaces | ForEach-Object {
-    #     $Output = $_.Instance.EndInvoke($_.IAResult)
-    #     New-Object psobject -Property @{
-    #         File = $file
-    #         FileCopied = $Output
-    #     }
-    # }
-
-    # $Results | Format-Table
-    <#Excel report#>
-    # $xlfile = "$env:Temp\testData.xlsx"
-    # $Results | Export-Excel $xlfile -WorkSheetname Exported -AutoSize -TableName Report -StartRow 2 -Show
-    # Close-ExcelPackage $excel -Show
-# }
-
-# $stopwatch.stop()
-
-# # show what we did.
-# [pscustomobject] @{
-#     csv_output = $OutputCsv
-#     total_files = $FileCount
-#     time_taken = $stopwatch.elapsed
-# }
