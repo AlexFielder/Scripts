c13551db4ed42a633c92803d5d286acd635e5f1b
alex fielder <alex.fielder@graitec.com>
2019-11-06 12:52:47 +0000

    modified to potentially use WatchedFolder

--- CmdLine/waitforWordpadToExitAndStartNotepad.cmd
+++ CmdLine/waitforWordpadToExitAndStartNotepad.cmd
@@ -1,19 +1,20 @@
 REM copied from here: https://stackoverflow.com/a/30171911/572634
 @ECHO OFF
 
-TASKKILL NOTEPAD
+REM TASKKILL /IM NOTEPAD.exe /f
 
-START "" "C:\Program Files\Windows NT\Accessories\wordpad.exe"
+START "" "\\PMT-CABMIGSQL01\Migrate\generic\megabatch_generic_scripts\megacopy.cmd" REM "C:\Program Files\Windows NT\Accessories\wordpad.exe"
 
 :LOOP
-tasklist | find /i "WORDPAD" >nul 2>&1
+tasklist | find /i "CEWatchFolder.exe" >nul 2>&1
 IF ERRORLEVEL 1 (
   GOTO CONTINUE
 ) ELSE (
-  ECHO Wordpad is still running
-  Timeout /T 5 /Nobreak
+  ECHO WatchedFolder is still running
+  Timeout /T 2 /Nobreak
   GOTO LOOP
 )
 
 :CONTINUE
-NOTEPAD
+cmd /c "D:\Dropbox\Scripts\CmdLine\waitforWordpadToExitAndStartNotepad.cmd"
+REM NOTEPAD
