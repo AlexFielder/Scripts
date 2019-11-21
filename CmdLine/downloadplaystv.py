f0bf6fd703ce1d1bba11b777f66f975c771ea56f
Alex Fielder <alex.fielder@gmail.com>
2019-11-21 21:09:06 +0000

    added timestamp code

--- CmdLine/downloadplaystv.py
+++ CmdLine/downloadplaystv.py
@@ -1,9 +1,13 @@
 #! /usr/bin/python3
 
+# original idea from this threddit: https://www.reddit.com/r/PlaysTV/comments/dz8oe4/psa_plays_is_shutting_down_download_your_videos/f877esw/
+# original code from this gist: https://gist.github.com/st0le/b6cb31272e1e03a7e1d479b2686d3e3e#file-playstv_videos_downloader-py
 # download all PlaysTv videos of a user
 # To find the user id, navigate to the your profile while logged in (IMPORTANT!)
 # View source of the page, In the <html> tag there's data-conf attribute. 
 # The json in there will have the user id under [login_user.id]
+import time
+import os
 from re import sub
 from json import load
 from urllib.request import urlretrieve, urlopen
@@ -32,6 +36,14 @@ def get_playstv_videos(user_id):
         print(title, url)
         try:
             urlretrieve(url, title)
+            try:
+                created = item["created"] / 1000
+                stamp = time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(created))
+                os.utime(title,stamp)
+                # command = 'SetFile -d \"' + stamp + '\" \"' + title + '\"'
+                # os.system(command)
+            except Exception as e:
+                pass
         except:
             print("Failed to download.")
 
