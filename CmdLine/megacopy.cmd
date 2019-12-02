0afd599b825492a85f15d38d9f0546d7fffe43a1
alex fielder <alex.fielder@graitec.com>
2019-12-02 17:10:56 +0000

    Update megacopy.cmd
    Now correctly works for any given source / destination combo

--- CmdLine/megacopy.cmd
+++ CmdLine/megacopy.cmd
@@ -1,10 +1,22 @@
-REM @echo off
+@echo off
 
-TIMEOUT 3
+REM TIMEOUT 3
 
-taskkill /f /im CEWatchFolder.exe
+REM taskkill /f /im CEWatchFolder.exe
 
-TIMEOUT 3
+REM TIMEOUT 3
+
+:LOOP
+tasklist | find /i "CEWatchFolder" >nul 2>&1
+IF ERRORLEVEL 1 (
+  GOTO CONTINUE
+) ELSE (
+  ECHO CEWatchFolder is still running
+  Timeout /T 10 /Nobreak
+  GOTO LOOP
+)
+
+:CONTINUE
 
 Setlocal EnableDelayedExpansion
 
@@ -14,16 +26,28 @@ set XMLProcessed=\\pmt-cabmigsql01\Migrate\OLAP\234814 - CAM\Processed
 
 set MaxLimit=100
 
-for /f "tokens=1* delims=[]" %%G in ('dir /A-D /B "%XMLSource%\*.*" ^| find /v /n ""') do (
-    copy /y "%XMLSource%\%%~nxH" "%XMLProcessing%"
-    move /y "%XMLSource%\%%~nxH" "%XMLProcessed%"
-    if %%G==%MaxLimit% goto :StartWatchedFolder REM exit /b 0
+REM setlocal enableextensions
+set count=0
+for %%x in ("%XMLSource%\*.*") do set /a count+=1
+REM endlocal
+echo %count%
+
+if %count% gtr 0 (
+    for /f "tokens=1* delims=[]" %%G in ('dir /A-D /B "%XMLSource%\*.*" ^| find /v /n ""') do (
+        copy /y "%XMLSource%\%%~nxH" "%XMLProcessing%"
+        move /y "%XMLSource%\%%~nxH" "%XMLProcessed%"
+        if %%G==%MaxLimit% goto :StartWatchedFolder REM exit /b 0
+    )
+
+) ELSE (
+    echo "Done processing..?"
+    exit
 )
 
 :StartWatchedFolder
 
-TIMEOUT 3
+    TIMEOUT 3
 
-start C:\watchedfolder\Application\CEWatchFolder.exe
-
+    start C:\watchedfolder\Application\CEWatchFolder.exe
+    start C:\watchedfolder\Application\megacopycad.cmd REM - rename this as appropriate
 exit
