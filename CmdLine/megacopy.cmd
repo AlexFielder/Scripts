8f815b6d12bfb8b3ac6109e67b52888bd42aae02
Alex Fielder <alex.fielder@gmail.com>
2019-12-02 14:38:36 +0000

    Added code from non-generic version

--- CmdLine/megacopy.cmd
+++ CmdLine/megacopy.cmd
@@ -1,13 +1,29 @@
-@echo off
+REM @echo off
 
-set XMLSource=C:\temp\Source
-set XMLProcessing=C:\temp\Processing
-set XMLProcessed=C:\temp\Processed
+TIMEOUT 3
 
-set MaxLimit=20
+taskkill /f /im CEWatchFolder.exe
+
+TIMEOUT 3
+
+Setlocal EnableDelayedExpansion
+
+set XMLSource=\\pmt-cabmigsql01\Migrate\OLAP\234814 - CAM\CAD
+set XMLProcessing=C:\watchedfolder\Queue\011cadOLAP
+set XMLProcessed=\\pmt-cabmigsql01\Migrate\OLAP\234814 - CAM\Processed
+
+set MaxLimit=100
 
 for /f "tokens=1* delims=[]" %%G in ('dir /A-D /B "%XMLSource%\*.*" ^| find /v /n ""') do (
     copy /y "%XMLSource%\%%~nxH" "%XMLProcessing%"
     move /y "%XMLSource%\%%~nxH" "%XMLProcessed%"
-    if %%G==%MaxLimit% exit /b 0
+    if %%G==%MaxLimit% goto :StartWatchedFolder REM exit /b 0
 )
+
+:StartWatchedFolder
+
+TIMEOUT 3
+
+start C:\watchedfolder\Application\CEWatchFolder.exe
+
+exit
