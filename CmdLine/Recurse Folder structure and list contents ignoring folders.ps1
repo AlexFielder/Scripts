d4b797a11acc39ebcbdbc14bbabe2a665de0398a
Alex Fielder <alex.fielder@gmail.com>
2020-01-21 16:06:51 +0000

    Added Recurse Folder script

---
+++ CmdLine/Recurse Folder structure and list contents ignoring folders.ps1
@@ -0,0 +1,15 @@
+Get-ChildItem -Path d:\ -Recurse |`
+ForEach-Object{
+    if (!($_.PSIsContainer)) {
+$Item = $_
+$Type = $_.Extension
+$Path = $_.FullName
+$Folder = $_.PSIsContainer
+$Age = $_.CreationTime
+
+$Path | Select-Object `
+    @{n="Name";e={$Item}},`
+    @{n="Created";e={$Age}},`
+    @{n="filePath";e={$Path}},`
+    @{n="Extension";e={if($Folder){"Folder"}else{$Type}}}`
+}}| convertto-csv -NoTypeInformation -Delimiter '|' | ForEach-Object { $_ -replace '"', ""} | out-file d:\results.csv -Force -Encoding UTF8
